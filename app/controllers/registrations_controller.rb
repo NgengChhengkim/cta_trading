3f46ddc76d50aa051f0bd1969e4cd1cb9e437ac5
yongsokheng <yongsokheng@ymail.com>
2016-08-01 13:22:26 +0700

    fix devise edit profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource resource, params
-    resource.update_without_password params
+    if params[:password].present?
+      resource.update_attributes params
+    else
+      resource.update_without_password params
+    end
   end
 end
