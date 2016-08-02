5a55ed5bceec56453727c0639b722636f67159ca
yongsokheng <yongsokheng@ymail.com>
2016-08-02 17:17:00 +0700

    add color migration

--- db/migrate/20160623142418_create_slide_images.rb
+++ db/migrate/20160623142418_create_slide_images.rb
@@ -3,13 +3,9 @@ class CreateSlideImages < ActiveRecord::Migration
     create_table :slide_images do |t|
       t.string :picture
       t.string :heading1
-      t.string :color_h1, default: "#fa03bb"
       t.string :heading2
-      t.string :color_h2, default: "#ffffff"
       t.string :heading3
-      t.string :color_h3, default: "#ffffff"
       t.string :heading4
-      t.string :color_h4, default: "#fa03bb"
       t.timestamps null: false
     end
   end
